4afb0f8ec0c373781771d430d24d038acf629513
Pavel Bandurin <91733314+flysputnik17@users.noreply.github.com>
2021-12-21 18:14:03 +0200

    Add files via upload

---
+++ week7/day1/main.py
@@ -0,0 +1,49 @@
+# def hello_user(username):
+#     print("hello " + username)
+
+
+# hello_user("rick")
+# hello_user("morty")
+
+
+# def say_hello(username, languge):
+#     if languge == "EN":
+#         print("hello "+username)
+#     elif languge == "FR":
+#         print("bonjur "+username)
+#     else:
+#         print("this language is not supported: "+languge)
+
+
+# say_hello(username="rick", languge="FR")
+# say_hello(username="PAVEL", languge="RU")
+# say_hello(username="morty", languge="EN")
+
+
+# name = "AVNER"
+
+
+# def username():
+#     print(name)
+
+
+# username()
+
+
+# def formating_name(first_name, last_name):
+#     full_name = first_name + " " + last_name
+#     return full_name.title()
+
+
+# mussician = formating_name("jimi", "hendrix")
+# print(mussician)
+
+
+# Write a function calculation() such that it can accept two variables and calculate the addition and subtraction of it. And also it must return both addition and subtraction in a single return call
+
+def calculation(a, b):
+     
+
+
+res = calculation(40, 10)
+print(res)
