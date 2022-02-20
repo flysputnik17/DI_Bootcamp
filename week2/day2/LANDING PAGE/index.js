6a42a27318c4a5214ae9a9304e1bb3484b71dfa6
Pavel Bandurin <91733314+flysputnik17@users.noreply.github.com>
2022-02-20 19:48:25 +0200

    Add files via upload

---
+++ week2/day2/LANDING PAGE/index.js
@@ -0,0 +1,5 @@
+alert ("hello  how are you ?")
+let age = prompt("how old are you ?")
+  if(age>18) {
+       alert("lets hit the road!!")
+  }
---
+++ week4/day 3/day 1/scrip.js
@@ -0,0 +1,14 @@
+// Exercise 1
+// 1. Create a structured html file linked to a js file
+
+// 2. Write a JS function that takes a parameter: myAge
+
+// 3. Console.log the age of my mum and my dad (my mum is twice my age, and my dad is 1.2 the age of my mum)
+
+// 4. Call the function
+let perent1 = ("mam");
+let perent2 = ("dad");
+function myAge(mam,dad){
+  console.log (myAge9
+    8)
+}
