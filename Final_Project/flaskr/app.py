86a73db43caa9e7e659678be785fba30a9955867
Pavel Bandurin <91733314+flysputnik17@users.noreply.github.com>
2022-02-20 19:47:15 +0200

    Add files via upload

---
+++ Final_Project/flaskr/app.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "index.html"
+
+
+app.run()
---
+++ Final_Project/flaskr/static/script.js
@@ -0,0 +1,7 @@
+//**Stiky Navigation */
+
+let navbar =$(".navbar ");
+
+$(window).scroll(function(){
+    console.log(window.innerHeight);
+});
